8cbb2628c6df49e68a8b9e3e3e4958d90bd233e7
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-04-26 07:57:32 -0500

    Hello Human assignment added

---
+++ _java/JavaSpring/HelloHuman/HelloHuman/src/main/java/com/example/demo/HelloHuman.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloHuman {
+		@RequestMapping("/")
+	    public String index(@RequestParam(value="FirstName", required=false) String firstName, @RequestParam(value="LastName", required=false) String lastName) {
+	        if(firstName==null) {
+	        	return "Hello Human";
+	        }
+	        else {
+	        	return "Hello: " + firstName + "      " + lastName;    	
+	        }	        	
+
+		}
+
+}
