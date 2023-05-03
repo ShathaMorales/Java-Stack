4390bd1bbd0384d64b288243e4e1e35f877fe08b
Shatha Morales <124023553+ShathaMorales@users.noreply.github.com>
2023-05-03 11:29:48 -0500

    "CareSoftInterface"

---
+++ _java/OOP Advanced/CareSoftInterface/CareSoftInterface/src/main/java/com/caresoft/clinicapp/CareSoftInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.caresoft.clinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareSoftInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareSoftInterfaceApplication.class, args);
+	}
+
+}
